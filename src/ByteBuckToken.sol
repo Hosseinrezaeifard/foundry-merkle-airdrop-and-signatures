b4298a943bfb94705cef7fd50eaddf7fc93556ed
HosseinRezaeiFard <huseinrezaeif99@gmail.com>
2025-06-29 14:18:38 -0700

    Commenting on bbt

--- src/ByteBuckToken.sol
+++ src/ByteBuckToken.sol
@@ -5,6 +5,10 @@ pragma solidity ^0.8.20;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+/*
+ * @author: 0xError
+ * @notice: This contract is a ByteBuck token contract
+ */
 contract ByteBuckToken is ERC20, Ownable {
     constructor() ERC20("ByteBuckToken", "BBT") Ownable(msg.sender) {}
 
