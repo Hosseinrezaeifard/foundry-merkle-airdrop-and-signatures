d7467c73d67998a2f9d2255eecf882c9cce899cb
HosseinRezaeiFard <huseinrezaeif99@gmail.com>
2025-06-29 13:55:20 -0700

    Implmenet custom token

---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+contract MerkleAirdrop {
+    // 1. We need to store a list of addresses
+    // 2. Some of them will be eligible for the airdrop
+}
